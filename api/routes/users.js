2392981c81cc24add742c470ae0450fcf452eb24
Rishu Singh <100370716+rishusingh11@users.noreply.github.com>
2022-11-06 02:05:35 +0530

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,9 +4,9 @@ const CryptoJS = require("crypto-js");
 const User = require("../models/User");
 const verify = require('../verifyToken');
 //UPDATE
-router.put("/:id", verify, async (req,res)=>{
-    if(req.user.id === req.params.id || req.user.isAdmin){
-        if(req.body.password){
+router.put("/:id", verify, async (req,res) => {
+    if(req.user.id === req.params.id || req.user.isAdmin) {
+        if(req.body.password) {
             req.body.password = CryptoJS.AES.encrypt(
                 req.body.password,
                 process.env.SECRET_KEY
@@ -21,7 +21,7 @@ router.put("/:id", verify, async (req,res)=>{
                 { new: true }
             );
             res.status(200).json(updatedUser);
-        } catch(err) {
+        } catch (err) {
             res.status(500).json(err)
         }
     } else {
