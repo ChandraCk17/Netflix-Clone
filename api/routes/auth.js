b21c4c0047a54c9a9d8f8bc1f352428d26546657
Rishu Singh <100370716+rishusingh11@users.noreply.github.com>
2022-11-06 01:39:33 +0530

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,11 +4,14 @@ const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
 //REGISTER
-router.post("/register", async (req, res) =>{
+router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt('req.body.password', process.env.SECRET_KEY).toString(),
+        password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.SECRET_KEY
+        ).toString(),
     });
 
     try{
@@ -25,18 +28,19 @@ router.post("/login", async (req, res) => {
         const user = await User.findOne({ email: req.body.email });
         !user && res.status(401).json("Wrong password or username!");
 
-        const bytes  = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
         const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
         originalPassword !== req.body.password &&
          res.status(401).json("Wrong password or username!");
         
             const accessToken = jwt.sign({id: user._id, isAdmin: user.isAdmin },
-                process.env.SECRET_KEY, { expiresIn: "5d" });
+                process.env.SECRET_KEY, 
+                { expiresIn: "5d" });
          const { password, ...info } = user._doc;
 
-        res.status(200).json({...info, accessToken });
-    }catch (err) {
+        res.status(200).json({ ...info, accessToken });
+    } catch (err) {
         res.status(500).json(err);
     }
 });
