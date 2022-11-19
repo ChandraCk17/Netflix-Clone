0e327a2dbc33f52baa4877b9c79df797129bb41e
Rishu Singh <100370716+rishusingh11@users.noreply.github.com>
2022-11-19 19:31:51 +0530

    Update Watch.jsx

--- ui-netflix/src/pages/watch/Watch.jsx
+++ ui-netflix/src/pages/watch/Watch.jsx
@@ -14,5 +14,5 @@ export default function watch() {
         controls 
         src="https://player.vimeo.com/progressive_redirect/playback/142611994/rendition/240p?loc=external&oauth2_token_id=1027659655&signature=d2b45d6a9d3d42e7a625415742485f00a8586e4f2109373903ae6c6558ee833e"/>
       </div>
-    )
+    );
   }
