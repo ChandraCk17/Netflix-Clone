07d0d06ecff79783030c2a2a2424aec7e4ffacab
Rishu Singh <100370716+rishusingh11@users.noreply.github.com>
2022-11-20 00:35:49 +0530

    Update movies.js

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -90,7 +90,7 @@ router.get("/", verify, async (req, res) => {
     if (req.user.isAdmin) {
         try {
             const movies = await Movie.find();
-            res.status(200).json(movies.reverse);
+            res.status(200).json(movies.reverse());
         } catch (err) {
             res.status(500).json(err);
         }
