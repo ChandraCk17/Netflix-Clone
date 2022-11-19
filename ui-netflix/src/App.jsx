352f22a392427e2fa17c6bf686ee8ee9750972fb
ChandraCk17 <mohitckdubey@gmail.com>
2022-11-20 02:39:06 +0530

    start

--- ui-netflix/src/App.jsx
+++ ui-netflix/src/App.jsx
@@ -3,14 +3,32 @@ import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
-  return <Home />;
+  return ( <Switch>
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/users">
+      <Users />
+    </Route>
+    <Route path="/">
+      <Home />
+    </Route>
+  </Switch>
+  );
+};
   //return <Register />;
   //return <Login />;
   //return <Watch />;
   
   
-};
+
 
 export default App;
