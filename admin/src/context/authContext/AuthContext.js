0e17a1677513a1d8f26f73dbfc498f2d6d56a2c1
ChandraCk17 <mohitckdubey@gmail.com>
2022-11-26 20:59:12 +0530

    update

--- admin/src/context/authContext/AuthContext.js
+++ admin/src/context/authContext/AuthContext.js
@@ -1,8 +1,8 @@
-import { createContext, useEffect, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
+import { createContext, useEffect, useReducer } from "react";
 
 const INITIAL_STATE = {
-    user: null,
+    user: JSON.parse(localStorage.getItem("user")) || null,
     isFetching: false,
     error: false,
 };
